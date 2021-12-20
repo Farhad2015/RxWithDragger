babe2bda1353cb665737a2dd4595869b2e788f16
Mahmudur Rahman <mrforhad1@gmail.com>
2021-12-20 23:45:09 +0600

    libary added to project

--- app/src/main/java/com/octaedges/advanceandroid/view/MainActivity.kt
+++ app/src/main/java/com/octaedges/advanceandroid/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.octaedges.advanceandroid
+package com.octaedges.advanceandroid.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.octaedges.advanceandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
