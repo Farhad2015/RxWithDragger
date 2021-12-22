e59eeadf0bafb0c68f33fecdf57921041df924bc
Mahmudur Rahman <mrforhad1@gmail.com>
2021-12-22 21:51:51 +0600

    view model update use mock data static

--- app/src/main/java/com/octaedges/advanceandroid/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/octaedges/advanceandroid/viewmodel/ListViewModel.kt
@@ -1,6 +1,35 @@
 package com.octaedges.advanceandroid.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.octaedges.advanceandroid.model.Country
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ"),
+            Country("CountryK")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
